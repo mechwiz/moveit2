8be4d005a50069892ea7e3a8f496f6df7b400e02
Vatan Aksoy Tezer <vatanaksoytezer@gmail.com>
2021-05-10 16:39:12 +0000

    Add missing TestGTestWaitForCompletion

--- moveit_ros/moveit_servo/test/launch/unit_test_servo_calcs.test.py
+++ moveit_ros/moveit_servo/test/launch/unit_test_servo_calcs.test.py
@@ -4,6 +4,7 @@ import launch_testing
 import os
 import sys
 import unittest
+from launch.actions import ExecuteProcess, TimerAction
 
 sys.path.append(os.path.dirname(__file__))
 from servo_launch_test_common import load_yaml
@@ -33,13 +34,24 @@ def generate_test_description():
 
     return (
         launch.LaunchDescription(
-            [test_binary_dir_arg, servo_gtest, launch_testing.actions.ReadyToTest()]
+            [
+                test_binary_dir_arg,
+                servo_gtest,
+                launch_testing.actions.ReadyToTest(),
+            ]
         ),
         {"servo_gtest": servo_gtest},
     )
 
 
+class TestGTestWaitForCompletion(unittest.TestCase):
+    # Waits for test to complete, then waits a bit to make sure result files are generated
+    def test_gtest_run_complete(self, servo_gtest):
+        self.proc_info.assertWaitForShutdown(servo_gtest, timeout=4000.0)
+
+
 @launch_testing.post_shutdown_test()
 class TestGTestProcessPostShutdown(unittest.TestCase):
+    # Checks if the test has been completed with acceptable exit codes (successful codes)
     def test_gtest_pass(self, proc_info, servo_gtest):
         launch_testing.asserts.assertExitCodes(proc_info, process=servo_gtest)

41b5aebe459268c33635ace7b9667b0a6f5ad46c
AndyZe <zelenak@picknik.ai>
2022-02-24 11:50:41 -0600

    Run duration extension until ::Finished, not ::Working

--- moveit_core/trajectory_processing/src/ruckig_traj_smoothing.cpp
+++ moveit_core/trajectory_processing/src/ruckig_traj_smoothing.cpp
@@ -167,7 +167,7 @@ bool RuckigSmoothing::applySmoothing(robot_trajectory::RobotTrajectory& trajecto
 
     // If ruckig failed, the duration of the seed trajectory likely wasn't long enough.
     // Try duration extension several times.
-    if (ruckig_result == ruckig::Result::Working)
+    if (ruckig_result == ruckig::Result::Finished)
     {
       smoothing_complete = true;
     }

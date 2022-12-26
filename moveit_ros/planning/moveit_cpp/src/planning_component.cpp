ea9ed00b78e2080156a7f58c2c1c4bc144c9840f
Michael Wiznitzer <michael.wiznitzer@resquared.com>
2022-12-26 18:33:04 -0500

    remove use_joint_limits

--- moveit_ros/planning/moveit_cpp/src/planning_component.cpp
+++ moveit_ros/planning/moveit_cpp/src/planning_component.cpp
@@ -147,7 +147,6 @@ planning_interface::MotionPlanResponse PlanningComponent::plan(const PlanRequest
   req.max_velocity_scaling_factor = parameters.max_velocity_scaling_factor;
   req.max_acceleration_scaling_factor = parameters.max_acceleration_scaling_factor;
   req.joint_limits = parameters.joint_limits;
-  req.use_joint_limits = parameters.use_joint_limits;
   req.skip_smoothing = parameters.skip_smoothing;
   if (workspace_parameters_set_)
     req.workspace_parameters = workspace_parameters_;
